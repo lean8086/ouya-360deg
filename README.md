d595b33a9dd07d129db62bbfd78e35d233802303
llinares <lean8086@gmail.com>
2012-12-08 02:07:39 -0300

    Add prefix-free

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-ouya-360deg
-===========
+# OUYA 360 degrees view
+3D representation of the OUYA game console built entirely in CSS.
+
+[See it now!](http://llinares.github.com/ouya-360deg/)
+
+## Contact
+CSS experiment by Leandro Linares [@lean8086](http://twitter.com/lean8086)
---
+++ ouya.js
@@ -0,0 +1,40 @@
+(function (win) {
+	'use strict';
+
+	var doc = win.document,
+
+		ouya = doc.getElementById('ouya'),
+
+		// Based on: http://lea.verou.me/2009/02/find-the-vendor-prefix-of-the-current-browser/
+		VENDOR_PREFIX = (function () {
+
+			var regex = /^(Webkit|Khtml|Moz|ms|O)(?=[A-Z])/,
+
+				styleDeclaration = doc.getElementsByTagName('script')[0].style,
+
+				prop;
+
+			for (prop in styleDeclaration) {
+				if (regex.test(prop)) {
+					return prop.match(regex)[0].toLowerCase();
+				}
+			}
+
+			// Nothing found so far? Webkit does not enumerate over the CSS properties of the style object.
+			// However (prop in style) returns the correct value, so we'll have to test for
+			// the precence of a specific property
+			if ('WebkitOpacity' in styleDeclaration) { return 'webkit'; }
+			if ('KhtmlOpacity' in styleDeclaration) { return 'khtml'; }
+
+			return '';
+		}()),
+
+		transform = (VENDOR_PREFIX !== '') ? '-' + VENDOR_PREFIX + '-transform' : 'transform';
+
+	doc.getElementsByClassName('display')[0].addEventListener('mousemove', function (event) {
+
+		ouya.style[transform] = 'rotateX(' + event.pageY + 'deg) rotateY(' + (event.pageX / 3) + 'deg) translateZ(-100px)';
+
+	});
+
+}(this));
